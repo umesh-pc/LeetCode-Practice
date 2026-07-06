3fae7a91b77538fefd8b8c3a8f7897679563780e
umesh-pc <umeshskannan@gmail.com>
2026-07-06 05:20:21 +0000

    [LeetCode Sync] Runtime - 3 ms (53.69%), Memory - 20.4 MB (40.66%)

--- solutions/0001-two-sum/solution.py
+++ solutions/0001-two-sum/solution.py
@@ -1,7 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n=len(nums)
-        for i in range(n):
-            for j in range(i+1,n):
-                if nums[i]+nums[j]==target:
-                     return i,j
+        visted={}
+        for i in range (len(nums)):
+            visted[nums[i]]=i
+        for i in range(len(nums)):
+            rem=target-nums[i]
+            if rem in visted and i != visted[rem]:
+                return[visted[rem],i]
